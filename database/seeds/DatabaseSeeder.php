b0689993e7a28da5338c95ec2f92f563aa8764c0
Francisco Ulloa <francisco.ulloa@ppm.com.ec>
2023-10-16 15:39:40 -0500

    [IMP] add seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(ProductSeeder::class);
+        // $this->call(ProductSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
