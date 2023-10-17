03766cfd6fafd0a3331faf347160e0e55087a54c
Francisco Ulloa <francisco.ulloa@ppm.com.ec>
2023-10-17 16:07:40 -0500

    [IMP, ADD] req and resources

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,30 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Resources\ProductCollection;
+use App\Http\Resources\ProductResource;
+use App\Http\Requests\ProductRequest;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function __construct()
     {
-        return Product::all();
+        // $this->middleware('auth:sanctum')->except(['index', 'show']);
     }
 
-    public function store(Request $request)
+    public function index()
+    {
+        return new ProductCollection(Product::paginate(5));
+    }
+
+    public function store(ProductRequest $request)
     {
         return Product::create($request->all());
     }
 
     public function show(Product $product)
     {
-        return $product;
+        return new ProductResource($product);;
     }
 
-    public function update(Request $request, Product $product)
+    public function update(ProductRequest $request, Product $product)
     {
         $product->update($request->all());
 
-        return $product;
+        return new ProductResource($product);
     }
 
     public function destroy(Product $product)
